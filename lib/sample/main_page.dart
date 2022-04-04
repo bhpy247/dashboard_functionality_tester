8cad7e02b5e060fdbce2ad84236f91090c3b2ff2
bhavisha <bhavishaparmar21@gmail.com>
2022-04-04 18:14:16 +0530

    hello commit

--- lib/sample/main_page.dart
+++ lib/sample/main_page.dart
@@ -20,6 +20,7 @@ class mainPage extends StatefulWidget {
 class _mainPageState extends State<mainPage> {
   List<String> dropdownList = [];
   String? dropdownValue;
+
   int i = 0;
 
   List<String> portList = [];
@@ -31,27 +32,16 @@ class _mainPageState extends State<mainPage> {
 
   @override
   void initState() {
-    initPorts();
     super.initState();
-    port = SerialPort("COM8");
+    port = SerialPort("COM13");
    // dropdownList = [];
   }
 
   @override
   void dispose() {
-    port.dispose();
     super.dispose();
   }
 
-  void initPorts() {
-    portList = SerialPort.availablePorts;
-    print("Port List:${portList}");
-    port = SerialPort(portList.isNotEmpty ? portList.first : "COM8");
-   // print("obje   $portList");
-
-
-    //print(port.isOpen);
-  }
 
   Future<void> setPort() async {
     print("set port");
@@ -60,57 +50,81 @@ class _mainPageState extends State<mainPage> {
     } else {
       print("port is close");
     }
+    port.openReadWrite();
 
-
-
-    port.config.baudRate = 115200;
+    startReading();
+    print("*********Before config***********");
+    print("BaudRate:${port.config.baudRate}");
+    print("Bits:${port.config.bits}");
+    print("StopBits:${port.config.stopBits}");
+    print("Parity:${port.config.parity}");
+    print("DTR:${port.config.dtr}");
+    port.config.baudRate = 9600;
     port.config.bits=8;
     port.config.stopBits=1;
     port.config.parity=0;
-    port.config.setFlowControl(SerialPortFlowControl.xonXoff);
-    /*port.config.xonXoff=SerialPortXonXoff.inOut;
-    port.config.rts=SerialPortRts.off;
-    port.config.dtr = SerialPortDtr.off;*/
-
-    //await Future.delayed(Duration(seconds: 1));
-
-    //port.config.dtr = 1;
-
-
-    port.openReadWrite();
+    port.config.dtr=1;
+    print("DTR after set:${port.config.dtr}");
+    await Future.delayed(Duration(seconds: 2));
+    port.config.dtr=0;
+    print("*********After config***********");
+    print("BaudRate:${port.config.baudRate}");
+    print("Bits:${port.config.bits}");
+    print("StopBits:${port.config.stopBits}");
+    print("Parity:${port.config.parity}");
     print("DTR:${port.config.dtr}");
-
-
-
-    /*port.config.rts=0;
-    port.config.dtr=0;*/
-
-    startReading();
-
-    print("RTS:${port.config.rts}");
-    Future.delayed(Duration(seconds: 5), () {
-      print("RTS:${port.config.rts}");
-    });
-
     print(port.isOpen);
+  }
+
+  Future<void> refresh() async {
+    print("refresh");
+    print("*********Before config***********");
+    print("BaudRate:${port.config.baudRate}");
+    print("Bits:${port.config.bits}");
+    print("StopBits:${port.config.stopBits}");
+    print("Parity:${port.config.parity}");
+    print("DTR:${port.config.dtr}");
+    port.config.dtr=0;
+    print("DTR after set:${port.config.dtr}");
+    await Future.delayed(Duration(seconds: 2));
+    port.config.dtr=1;
+    print("*********After config***********");
+    print("BaudRate:${port.config.baudRate}");
+    print("Bits:${port.config.bits}");
+    print("StopBits:${port.config.stopBits}");
+    print("Parity:${port.config.parity}");
+    print("DTR:${port.config.dtr}");
     print(port.isOpen);
   }
 
   void startReading()  {
-    reader = SerialPortReader(port,timeout: 5000);
-
-    /*reader.stream.forEach((element) {
-      print('received: ${String.fromCharCodes(element)}');
-    }).asStream();*/
-
+    reader = SerialPortReader(port,timeout: -1);
     reader.stream.listen(
     (data) {
-      print('received Data in Stream: $data');
-      print('received: ${String.fromCharCodes(data)}');
-      // displayData += data.toString();
-      //print('received: $data');
+      try {
+        String receivedData="";
+        print("data $data");
+      data.forEach((element) {
+        if (element != 10) {
+          //print("element $element");
+          receivedData += String.fromCharCode(element);
+          //print("final Data in if: $receivedData");
+        }
+        else {
+          print("final Data: $receivedData");
+          receivedData="";
+        }
+      });
+    }
+    catch(e)
+    {
+      print("error: $e");
+    }
+
+
     },
-    onDone: (){print("on done");}
+    onDone: (){
+      print("on done");}
     ).onError((e){
       print("on Error $e");
     });
@@ -118,25 +132,33 @@ class _mainPageState extends State<mainPage> {
     print("start reading");
   }
 
-  void sendData() {
+  void closeReading(){
+    reader.close();
+  }
+  void closePort(){
+    port.dispose();
+    port.close();
+  }
+
+  void sendData(String cmd) async{
     try {
-      String data = "\n\rCOP11CE\n";
+
+      String data = "\n\rXX---CE\n";
 
       List<int> sendData = [];
 
-      //   sendData.add(int.parse('\n'));
-      // sendData.add(int.parse('\r'));
-
       for (int i = 0; i < data.length; i++) {
-        //  sendData.add(int.parse(data[i]));
+        sendData = data[i].codeUnits;
+        int b=port.write(Uint8List.fromList(sendData));
+        print("Send done ${(sendData)}: $b");
       }
       //  sendData.add(int.parse('C'));
       //  sendData.add(int.parse('E'));
       // sendData.add(int.parse('\n'));
 
-      sendData = data.codeUnits;
-      port.write(Uint8List.fromList(sendData));
-      print("Send done $sendData");
+     /*sendData = data.codeUnits;
+      int b=port.write(Uint8List.fromList(sendData));
+      print("Send done $sendData: $b");*/
     } catch (e) {
       print("Error  : $e");
     }
@@ -153,18 +175,7 @@ class _mainPageState extends State<mainPage> {
         ),
         backgroundColor: Colors.deepPurple,
       ),
-      body: Column(
-        children: [
-          const SizedBox(
-            height: 40,
-          ),
-          getDropdowRow(),
-          const SizedBox(
-            height: 40,
-          ),
-          Expanded(child: getListColumn())
-        ],
-      ),
+      body: Center(child: getListColumn()),
     );
   }
 
@@ -211,13 +222,13 @@ class _mainPageState extends State<mainPage> {
 
   Widget getListColumn() {
     return Column(
-      mainAxisAlignment: MainAxisAlignment.start,
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
       children: [
         MaterialButton(
           color: Colors.deepPurple,
           onPressed: () async {
             setPort();
-
             /*socket = await Socket.connect("localhost", 6667);
             Uint8List data = await socket!.first;
             print("Data:${data}");
@@ -234,6 +245,7 @@ class _mainPageState extends State<mainPage> {
                     fontSize: 17),
               )),
         ),
+        SizedBox(height: 20,),
         MaterialButton(
           color: Colors.deepPurple,
           onPressed: () async {
@@ -256,11 +268,12 @@ class _mainPageState extends State<mainPage> {
                     fontSize: 17),
               )),
         ),
+        SizedBox(height: 20,),
         MaterialButton(
           color: Colors.deepPurple,
           onPressed: () async {
-            //startReading();
-            if(socket != null) {
+            startReading();
+            /*if(socket != null) {
               print("In Socket Write");
               //socket!.write(utf8.encode("In Socker Write"));
               socket!.write("Dishant\n");
@@ -270,7 +283,7 @@ class _mainPageState extends State<mainPage> {
 
               socket!.write("Agrawal\n");
               print("In Socket Write Completed");
-            }
+            }*/
           },
           child: Container(
               padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 9),
@@ -282,10 +295,14 @@ class _mainPageState extends State<mainPage> {
                     fontSize: 17),
               )),
         ),
+        SizedBox(height: 20,),
         MaterialButton(
           color: Colors.deepPurple,
           onPressed: () {
-            sendData();
+            //sendData("FMAE111110000000000");
+            //sendData("FMS--");
+            sendData("XX---");
+
           },
           child: Container(
               padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 9),
@@ -297,31 +314,35 @@ class _mainPageState extends State<mainPage> {
                     fontSize: 17),
               )),
         ),
+        SizedBox(height: 20,),
         MaterialButton(
           color: Colors.deepPurple,
-          onPressed: () {
-
+          onPressed: () async{
             port.config.dtr=1;
-            //port.refreshConfig();
-
             print("off");
-            Future.delayed(const Duration(seconds: 3),(){
-              port.config.dtr=0;
-              print("on");
-             // port.refreshConfig();
-            });
+            await Future.delayed(Duration(seconds: 2));
+            port.config.dtr=0;
+            print("on");
+            print("*********After DTR config***********");
+            print("BaudRate:${port.config.baudRate}");
+            print("Bits:${port.config.bits}");
+            print("StopBits:${port.config.stopBits}");
+            print("Parity:${port.config.parity}");
+            print("DTR:${port.config.dtr}");
+            print(port.isOpen);
 
           },
           child: Container(
               padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 9),
               child: const Text(
-                "Restart port",
+                "Restart DTR",
                 style: const TextStyle(
                     color: Colors.white,
                     fontWeight: FontWeight.bold,
                     fontSize: 17),
               )),
         ),
+        SizedBox(height: 20,),
         MaterialButton(
           color: Colors.deepPurple,
           onPressed: () {
@@ -351,6 +372,55 @@ class _mainPageState extends State<mainPage> {
                     fontSize: 17),
               )),
         ),
+        SizedBox(height: 20,),
+        MaterialButton(
+          color: Colors.deepPurple,
+          onPressed: () {
+            closeReading();
+          },
+          child: Container(
+              padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 9),
+              child: const Text(
+                "Close read",
+                style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 17),
+              )),
+        ),
+        SizedBox(height: 20,),
+        MaterialButton(
+          color: Colors.deepPurple,
+          onPressed: () {
+            refresh();
+          },
+          child: Container(
+              padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 9),
+              child: const Text(
+                "Refresh",
+                style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 17),
+              )),
+        ),
+        SizedBox(height: 20,),
+        MaterialButton(
+          color: Colors.deepPurple,
+          onPressed: () {
+            closePort();
+          },
+          child: Container(
+              padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 9),
+              child: const Text(
+                "Close port",
+                style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 17),
+              )),
+        ),
+
 
         // SizedBox(
         //   height: 10,
